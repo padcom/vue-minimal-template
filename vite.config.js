9fe718d8e93230ec91a69fe18783c9d955f554fc
Matthias Hryniszak <padcom@gmail.com>
2023-10-31 00:58:21 +0100

    Replaced custom defines with .env entry defining app name

--- vite.config.js
+++ vite.config.js
@@ -4,32 +4,17 @@ import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import autoprefixer from 'autoprefixer'
 
-import pkg from './package.json'
-
-/**
- * Compose an object of package.json fields for definition of `import.meta.env.XXX` constants
- */
-function getPkgDefines() {
-  return Object.keys(pkg)
-    .filter(key => typeof pkg[key] !== 'function')
-    .map(key => ({ [`import.meta.env.PACKAGE_${key.toUpperCase()}`]: JSON.stringify(pkg[key]) }))
-    .reduce((acc, entry) => ({ ...acc, ...entry }), {})
-}
-
 export default defineConfig({
-  define: {
-    ...getPkgDefines(),
-  },
   plugins: [
-    svg({
-      defaultImport: 'component',
-    }),
     vue({
       template: {
         compilerOptions: {
           isCustomElement: tag => tag.includes('-'),
         },
       },
+    }),
+    svg({
+      defaultImport: 'component',
     }),
     eslint({
       lintOnStart: false,
