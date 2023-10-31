16b99c2f7584ae91c9155dc885be4faab2d64b14
Matthias Hryniszak <padcom@gmail.com>
2023-10-31 01:12:05 +0100

    Added HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <h1>{{ message }}</h1>
+  <HelloWorld />
+  <p class="note">Now, delete me and get to work!</p>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-
-const message = ref('Hello, world!')
+import HelloWorld from './components/HelloWorld.vue'
 </script>
+
+<style lang="postcss" scoped>
+.note {
+  margin-top: 2.5rem;
+}
+</style>
