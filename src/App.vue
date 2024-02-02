fcadaec63f2221b37458b9531281387afc47894c
Matthias Hryniszak <padcom@gmail.com>
2024-02-02 09:27:02 +0100

    Updated the HelloWorld component to show a demonstration of using defineModel

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <HelloWorld />
+  <HelloWorld v-model="message" />
   <p class="note">Now, delete me and get to work!</p>
 </template>
 
 <script lang="ts" setup>
+import { ref, watch } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+
+const message = ref('(click to change)')
+
+watch(message, value => { console.log('Changed to', value) })
 </script>
 
 <style lang="postcss" scoped>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,17 @@
 <template>
-  <h1>Hello, world! {{ message }}</h1>
+  <h1 @click="message = 'Changed!'">Hello, world! {{ message }}</h1>
 </template>
 
 <script lang="ts" setup>
-defineProps({
-  message: { type: String, default: 'from HelloWorld.vue' },
-})
+import { defineModel } from 'vue'
+
+const message = defineModel<string>({ type: String, default: 'from HelloWorld.vue' })
 </script>
 
 <style lang="postcss" scoped>
 h1 {
   font-size: 1.875rem;
   line-height: 2.25rem;
+  cursor: pointer;
 }
 </style>
