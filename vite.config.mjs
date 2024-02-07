79826f892f2cea993761981b472f2cae334959b5
Matthias Hryniszak <padcom@gmail.com>
2024-02-07 08:45:02 +0100

    Added @ aliast to /src because vscode can use it

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,3 +1,4 @@
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import svg from 'vite-svg-loader'
 import vue from '@vitejs/plugin-vue'
@@ -33,6 +34,11 @@ export default defineConfig({
       plugins: [
         autoprefixer(),
       ],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
