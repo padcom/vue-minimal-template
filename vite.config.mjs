17bc294e29fd990693da12f694a753fa4961a06f
Matthias Hryniszak <padcom@gmail.com>
2025-05-13 16:44:24 +0200

    Dependency updates

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,10 @@
 /* eslint-env node */
 import { defineBaseConfig } from '@padcom/vite-config-default'
-import { defineVueBaseConfig } from '@padcom/vite-config-vue'
+import { defineVueBaseConfig, defineVendorChunkConfig } from '@padcom/vite-config-vue'
 
 import pkg from './package.json' with { type: 'json' }
 
-export default defineBaseConfig(pkg, defineVueBaseConfig({
+export default defineBaseConfig(pkg, defineVendorChunkConfig(defineVueBaseConfig({
   define: {
     // Enable / disable Options API support
     __VUE_OPTIONS_API__: true,
@@ -13,4 +13,4 @@ export default defineBaseConfig(pkg, defineVueBaseConfig({
     // Enable / disable detailed warnings for hydration mismatches in production
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
   },
-}))
+})))
